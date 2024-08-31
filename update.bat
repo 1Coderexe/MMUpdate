1c2a83622be58eb04e40c7b98848c12b3db29fa5
Coderexe <76922985+1Coderexe@users.noreply.github.com>
2024-08-30 18:12:01 -0700

    Update update.bat

--- update.bat
+++ update.bat
@@ -1,12 +1,10 @@
 @echo off
 setlocal
 
-:: OO LAH LAH CHANGEDDDDD:DDDDD 
-
 :: Define URLs and filenames
 set VERSION_URL=https://raw.githubusercontent.com/1Coderexe/MMUpdate/main/version.txt
 set UPDATE_URL=https://raw.githubusercontent.com/1Coderexe/MMUpdate/main/update.bat
-set CURRENT_VERSION=1.0.0
+set CURRENT_VERSION=0.1
 set TEMP_FILE=temp_update.bat
 set SCRIPT_NAME=self_update.bat
 set RESTART_FILE=temp_restart.bat
